3e11a6df4e13e51cda568cbdc220729ba7d659b6
Vaibhav Kaushik <vaibhav211102@gmail.com>
2026-09-01 12:21:44 +0530

    STRING IN JAVA

---
+++ src/Strings_in_Java/first_string_program.java
@@ -0,0 +1,9 @@
+package Strings_in_Java;
+
+public class first_string_program {
+    public static void main(String[] args) {
+        String s = "Vaibhav";
+
+        System.out.println(s);
+    }
+}
